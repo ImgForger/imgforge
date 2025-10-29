5da7ddd15d62a2a9e04a86da760dd1458a3fc4c9
Rafi <mr.k779@outlook.com>
2025-10-28 23:53:59 -0400

    Update deploy-systemd.sh

--- deployment/deploy-systemd.sh
+++ deployment/deploy-systemd.sh
@@ -619,9 +619,10 @@ install_grafana() {
 
     case $PKG_MANAGER in
         apt)
-            sudo apt-get install -y software-properties-common
-            sudo wget -q -O /usr/share/keyrings/grafana.key https://apt.grafana.com/gpg.key
-            echo "deb [signed-by=/usr/share/keyrings/grafana.key] https://apt.grafana.com stable main" | sudo tee /etc/apt/sources.list.d/grafana.list
+            sudo apt-get install -y apt-transport-https software-properties-common
+            sudo mkdir -p /etc/apt/keyrings/
+            wget -q -O - https://apt.grafana.com/gpg.key | gpg --dearmor | sudo tee /etc/apt/keyrings/grafana.gpg > /dev/null
+            echo "deb [signed-by=/etc/apt/keyrings/grafana.gpg] https://apt.grafana.com stable main" | sudo tee -a /etc/apt/sources.list.d/grafana.list
             sudo apt-get update -qq
             sudo apt-get install -y grafana
             ;;
