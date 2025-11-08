6ebe1c669e788da34406910a6941984a05748502
Rafi <mr.k779@outlook.com>
2025-11-08 15:04:24 -0500

    Update middleware.rs

--- src/middleware.rs
+++ src/middleware.rs
@@ -23,13 +23,13 @@ fn generate_request_id() -> String {
 
 pub fn format_to_content_type(format: &str) -> &'static str {
     match format {
-        "png" => "image/png",
-        "webp" => "image/webp",
-        "gif" => "image/gif",
-        "tiff" => "image/tiff",
-        "avif" => "image/avif",
-        "heif" => "image/heif",
-        "jpeg" | "jpg" => "image/jpeg",
+        "png" | "image/png" => "image/png",
+        "webp" | "image/webp" => "image/webp",
+        "gif" | "image/gif" => "image/gif",
+        "tiff" | "image/tiff" => "image/tiff",
+        "avif" | "image/avif" => "image/avif",
+        "heif" | "image/heif" => "image/heif",
+        "jpeg" | "jpg" | "image/jpeg" => "image/jpeg",
         _ => "image/jpeg",
     }
 }
